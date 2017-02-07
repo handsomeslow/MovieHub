f6507f910f5e8b3761be8470c7b405ead0181492
Jun <xujun02@58ganji.com>
2017-02-07 19:17:57 +0800

    add search activity

---
+++ app/src/main/java/com/jx/moviehub/activity/SearchMovieActivity.java
@@ -0,0 +1,15 @@
+package com.jx.moviehub.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jx.moviehub.R;
+
+public class SearchMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qurey_movie);
+    }
+}
